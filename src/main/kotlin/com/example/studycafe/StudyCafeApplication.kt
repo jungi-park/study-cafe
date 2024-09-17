663b7c3e6f63571be6ee5ed7d65b8dc2a601b99f
박준기 <qmqqqqm@gmail.com>
2024-09-17 19:33:05 +0900

    init study-cafe

---
+++ src/main/kotlin/com/example/studycafe/StudyCafeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.studycafe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyCafeApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudyCafeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/studycafe/StudyCafeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.studycafe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyCafeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
