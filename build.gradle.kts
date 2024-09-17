375c83b199f1bfdcb9a8388860e718c8ed7b916a
박준기 <qmqqqqm@gmail.com>
2024-09-17 21:16:30 +0900

    kafka publish test

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.kafka:spring-kafka")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
---
+++ src/test/kotlin/com/example/studycafe/kafka/kafkaTest.kt
@@ -0,0 +1,20 @@
+package com.example.studycafe.kafka
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestConstructor
+import kotlin.test.Test
+
+@SpringBootTest
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+class kafkaTest(
+    private val kafkaTemplate: KafkaTemplate<String, String>
+) {
+
+    @Test
+    fun `카프카 데이터 전송 테스트`() {
+        val message = "데이터 전송 테스트중 1"
+        kafkaTemplate.send("my-topic", message)
+    }
+}
