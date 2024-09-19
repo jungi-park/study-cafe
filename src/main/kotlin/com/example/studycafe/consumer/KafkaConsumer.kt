a82e04e3448317604c2ff8eb736987a4c2ffdcf6
박준기 <qmqqqqm@gmail.com>
2024-09-19 22:13:34 +0900

    kafka subscribe test 성공

---
+++ src/main/kotlin/com/example/studycafe/consumer/KafkaConsumer.kt
@@ -0,0 +1,16 @@
+package com.example.studycafe.consumer
+
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.stereotype.Service
+
+@Service
+class KafkaConsumer {
+
+    var receivedMessage = ""
+
+    @KafkaListener(topics = ["my-topic"], groupId = "parkJunGi")
+    fun listen(message: String) {
+        receivedMessage = message
+        println("Received message: $message")
+    }
+}
--- src/test/kotlin/com/example/studycafe/kafka/kafkaTest.kt
+++ src/test/kotlin/com/example/studycafe/kafka/kafkaTest.kt
@@ -1,19 +1,35 @@
 package com.example.studycafe.kafka
 
+import com.example.studycafe.consumer.KafkaConsumer
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.test.context.TestConstructor
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 import kotlin.test.Test
 
 @SpringBootTest
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 class kafkaTest(
-    private val kafkaTemplate: KafkaTemplate<String, String>
+    private val kafkaTemplate: KafkaTemplate<String, String>,
+    private val kafkaConsumer: KafkaConsumer
 ) {
 
     @Test
     fun `카프카 데이터 전송 테스트`() {
         val message = "데이터 전송 테스트중 1"
         kafkaTemplate.send("my-topic", message)
+    }
+
+    @Test
+    fun listen() {
+        kafkaTemplate.send("my-topic", "테스트 메시지")
+
+        // 메시지가 수신될 때까지 대기
+        Thread.sleep(1000)
+
+        println("kafkaConsumer == ${kafkaConsumer.receivedMessage}")
     }
 }
